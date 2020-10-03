e3c721f649afa9f4e0fc056eae9777631dab325e
taka <0bn644063264k0f@ezweb.ne.jp>
2020-10-03 11:02:21 +0000

    add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do |n|
+  User.create(
+    email: "test#{n + 1}@test.com",
+    name: "テスト太郎#{n + 1}",
+    password: "test#{n + 1}",
+    password_confirmation: "test#{n + 1}",
+    teacher: false
+  )
+end
+
+5.times do |n|
+  User.create(
+    email: "test#{n + 1}@test.com",
+    name: "テスト太郎先生#{n + 1}",
+    password: "test#{n + 1}",
+    password_confirmation: "test#{n + 1}",
+    teacher: true
+  )
+end
+
+30.times do |n|
+  Word.create(
+    content: "word#{n + 1}",
+    meaning: "meaning#{n + 1}",
+    example: "example#{n + 1}",
+    eiken: true,
+    toeic: false
+    )
+end
+
+30.times do |n|
+  Word.create(
+    content: "word#{n + 1}",
+    meaning: "meaning#{n + 1}",
+    example: "example#{n + 1}",
+    eiken: false,
+    toeic: true
+    )
+end
